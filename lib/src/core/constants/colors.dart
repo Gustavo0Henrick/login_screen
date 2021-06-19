0f037a3d1fe143d168b7ab936deceb79f4e8e064
Gustavo Henrick <gustavo08henrick@gmail.com>
2021-06-19 15:35:50 -0300

    home appbar

--- lib/src/core/constants/colors.dart
+++ lib/src/core/constants/colors.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 
 abstract class CustomColors {
-  static const Color mainColor = Colors.orange;
-  static const Color white = Colors.white;
+  static const Color mainColor = Color(0xFFFF9800);
+  static const Color darkOrange = Color(0xFFEF6C00);
+  static const Color deepOrange = Color(0xFFBF360C);
+  static const Color white = Color(0xFFFFFFFF);
   static const Color transparent = Colors.transparent;
-  static const Color black = Colors.black;
-  static const Color red = Colors.red;
+  static const Color black = Color(0xFF000000);
+  static const Color red = Color(0xFFF44336);
 }
---
+++ lib/src/pages/home/home widgets/home_appbar.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:login_screen/src/core/constants/colors.dart';
+
+AppBar getHomeAppBar() {
+  return AppBar(
+    title: Text("Loja"),
+    centerTitle: true,
+    backgroundColor: CustomColors.deepOrange,
+    leading: IconButton(
+      onPressed: () {},
+      icon: Icon(
+        Icons.menu,
+        color: CustomColors.white,
+      ),
+    ),
+    actions: [
+      IconButton(
+        icon: Icon(
+          Icons.more_vert_rounded,
+          color: CustomColors.white,
+        ),
+        onPressed: () {},
+      ),
+    ],
+  );
+}
