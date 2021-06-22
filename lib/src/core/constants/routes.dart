5520ea3dfe25339b7b918431d99d9997f4428d88
Gustavo Henrick <gustavo08henrick@gmail.com>
2021-06-22 19:33:20 -0300

    att

--- lib/src/core/constants/routes.dart
+++ lib/src/core/constants/routes.dart
@@ -1,6 +1,6 @@
 class Routes {
   static const signUp =
-      "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBBr50PuwJacfZknqEW-yLBNNWd8Mbl3y4";
+      "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=KEY";
   static const signIn =
-      "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBBr50PuwJacfZknqEW-yLBNNWd8Mbl3y4";
+      "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=KEY";
 }
